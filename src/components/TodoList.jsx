94a9d6bfba7a13798eb7744e271a298f749808ab
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2026-06-22 17:25:20 +0600

    Refactor TodoList component to integrate fetchTodos on mount and streamline filtering logic

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,30 +1,35 @@
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import TodoItem from './TodoItem';
-
-function filterTodosByStatus(todos, status) {
-  switch (status) {
-    case 'complete':
-      return todos.filter((todo) => todo.complete);
-    case 'incomplete':
-      return todos.filter((todo) => !todo.complete);
-    default:
-      return todos;
-  }
-}
-
-function filterTodosByColors(todos, colors) {
-  if (colors.length > 0) {
-    return todos.filter((todo) => colors.includes(todo?.color));
-  }
-  return todos;
-}
+import {useEffect} from 'react';
+import fetchTodos from '../redux/todos/thunk/fetchTodos';
 
 export default function TodoList() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTodos);
+  }, [dispatch]);
+
   const todos = useSelector((state) => state.todos);
   const {status, colors} = useSelector((state) => state.filters);
 
-  let filteredTodos = filterTodosByStatus(todos, status);
-  filteredTodos = filterTodosByColors(filteredTodos, colors);
+  const filteredTodos = todos
+    .filter((todo) => {
+      switch (status) {
+        case 'complete':
+          return todo.complete;
+        case 'incomplete':
+          return !todo.complete;
+        default:
+          return true;
+      }
+    })
+    .filter((todo) => {
+      if (colors.length > 0) {
+        return colors.includes(todo?.color);
+      }
+      return true;
+    });
 
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-75 overflow-y-auto">
